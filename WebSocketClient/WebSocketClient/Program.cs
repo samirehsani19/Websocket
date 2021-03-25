941d208b3571aade7cf31df6932e1a77bba900f9
Samir Ehsani <samir.ehsani@learnet.se>
2021-03-25 19:36:27 +0100

    websocket server with client based on TCP connection

---
+++ WebSocketClient/WebSocketClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        private static Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            LoopConnect();
+            SendLoop();
+            Console.ReadKey();
+        }
+
+        private static void SendLoop()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a request");
+                var kP = Console.ReadKey();
+                var message = kP.KeyChar;
+
+                byte[] buffer = Encoding.ASCII.GetBytes(message.ToString());
+                clientSocket.Send(buffer);
+
+                byte[] recieveBuffer = new byte[1024];
+                int rec = clientSocket.Receive(recieveBuffer);
+                byte[] data = new byte[rec];
+                Array.Copy(recieveBuffer, data, rec);
+                Console.WriteLine($"\nText received from Server: {Encoding.ASCII.GetString(data)}");
+            }
+        }
+
+        private static void LoopConnect()
+        {
+            int attempts = 0;
+            while (!clientSocket.Connected)
+            {
+                try
+                {
+                    attempts++;
+                    clientSocket.Connect(IPAddress.Loopback, 100);
+                }
+                catch(SocketException)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Connections attemps: {attempts}");
+                }
+            }
+            Console.Clear();
+            Console.WriteLine($"{clientSocket.RemoteEndPoint}: Connected");
+        }
+    }
+}
---
+++ websocket/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace websocket
+{
+    class Program
+    {
+        private static byte[] buffer = new byte[1024];
+        private static List<Socket> clientSockets = new();
+        private static Socket serverSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadKey(); 
+        }
+        
+        private static void SetupServer()
+        {
+            Console.WriteLine("Setting up server....");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            serverSocket.Listen(5);
+
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+        
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket = serverSocket.EndAccept(AR);
+            clientSockets.Add(socket);
+            Console.WriteLine($"{serverSocket.LocalEndPoint}: connected");
+            socket.BeginReceive(buffer, 0 , buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[received];
+            Array.Copy(buffer, dataBuf, received);
+             
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine($"Text recieved from Client: \t{text} \t {DateTime.Now.ToLongTimeString()}");
+
+            string response = $"Hello from Server \t {DateTime.Now.ToLongTimeString()}";
+            byte[] data = Encoding.ASCII.GetBytes(response);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+
+        }
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+    }
+}
